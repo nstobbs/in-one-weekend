dc2414886c15bbb1cea36be68ee7bf111a564057
Nathan Stobbs <nstobbs97@gmail.com>
2024-11-01 22:23:46 +0000

    Image Hello World

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,26 @@
 
 int main()
 {
-    std::cout << "hello" << std::endl;
-    return 0;
+    // Image
+    int imagePlaneHeight = 256;
+    int imagePlaneWidth = 256;
+
+    // Render
+    std::cout << "P3\n" << imagePlaneWidth << ' ' << imagePlaneHeight << "\n255\n";
+    
+    for (int y = 0; y < imagePlaneHeight; y++)
+    {
+        for (int x = 0; x < imagePlaneWidth; x++)
+        {
+            auto red = float(x) / (imagePlaneWidth - 1);
+            auto green = float(y) / (imagePlaneHeight - 1);
+            auto blue = 0.0f;
+
+            int iRed = int(255.999 * red);
+            int iGreen = int(255.999 * green);
+            int iBlue = int(255.999 * blue);
+
+            std::cout << iRed << ' ' << iGreen << ' ' << iBlue << '\n';
+        };
+    };
 }
